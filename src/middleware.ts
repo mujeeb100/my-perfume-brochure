0db836fa87829bba990224100af46eb31c0fd597
mujeeb-mythik <mujeebur.ansari@mythik.ai>
2025-08-10 03:59:31 +0530

    fix the deployment error on vercel

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { auth } from "./auth";
+// import { NextRequest } from "next/server";
 
-export default auth((req:any) => {
+export default auth((req) => {
   if (!req.auth && req.nextUrl.pathname.startsWith("/edit-photo")) {
     return Response.redirect(new URL("/login", req.nextUrl));
   }
--- src/providers.tsx
+++ src/providers.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 
+import { Session } from "next-auth";
+
 export function Providers({ children, session }: { 
   children: React.ReactNode;
-  session: any;
+  session: Session | null;
 }) {
   return (
     <SessionProvider session={session}>
